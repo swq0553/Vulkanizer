3e64505c1b765d042408e9c13085c0993e39566b
Ryp <ryp.sqrt@gmail.com>
2015-03-04 11:57:15 +0000

    Add basic structure

---
+++ vulzr/version.hpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+/// Vulcanizer
+///
+/// Copyright (c) 2015 Thibault Schueller
+/// This file is distributed under the MIT License
+///
+/// @file version.hpp
+/// @author Thibault Schueller <ryp.sqrt@gmail.com>
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef VULZR_VERSION_INCLUDED
+#define VULZR_VERSION_INCLUDED
+
+#define VULZR_VERSION        10
+#define VULZR_VERSION_MAJOR  0
+#define VULZR_VERSION_MINOR  1
+#define VULZR_VERSION_PATCH  0
+
+#endif // VULZR_VERSION_INCLUDED
---
+++ vulzr/vulzr.hpp
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////////////////
+/// Vulcanizer
+///
+/// Copyright (c) 2015 Thibault Schueller
+/// This file is distributed under the MIT License
+///
+/// @file vulzr.hpp
+/// @author Thibault Schueller <ryp.sqrt@gmail.com>
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef VULZR_INCLUDED
+#define VULZR_INCLUDED
+
+#include <vulzr/version.hpp>
+
+#endif // VULZR_INCLUDED
